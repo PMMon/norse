f8d277c61857e517e0da9e6391d052fa6c258fba
PMMon <philipp.mondorf@googlemail.com>
2021-08-23 11:14:20 +0200

    Minkowski LIF Cell

--- norse/task/minkowski/lif.py
+++ norse/task/minkowski/lif.py
@@ -17,25 +17,34 @@ class MinkowskiLIFCell(ME.MinkowskiModuleBase):
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.module = LIFCell(*args, **kwargs)
-        self.pruning = ME.MinkowskiPruning()
         self.union = ME.MinkowskiUnion()
 
     def forward(self, input, state):
         if state is not None:
+            """
             # Align state with input
             zero_state_tensor = ME.SparseTensor(
                 features=torch.zeros_like(state.v.F),
-                coordinates=state.v.coordinates,
+                coordinates=state.v.C,
                 coordinate_manager=input.coordinate_manager,
             )
-            v_tensor = self.union(
-                input,
-                ME.SparseTensor(
+            print("v: " + str(state.v))
+            v_in = ME.SparseTensor(
                     features=state.v.F,
                     coordinates=state.v.C,
                     coordinate_manager=input.coordinate_manager,
-                ),
+                )
+            print("v_in: " + str(v_in))
+            #print("v coordinates: " + str(state.v.C))
+            #print("v features: " + str(state.v.F))
+            print("input: " + str(input))
+            #print("input coordinates: " + str(input.C))
+            #print("input features: " + str(input.F))
+            v_tensor = self.union(
+                input,
+                v_in
             )
+            print("v_tensor: " + str(v_tensor))
             i_tensor = self.union(
                 input,
                 ME.SparseTensor(
@@ -46,31 +55,55 @@ class MinkowskiLIFCell(ME.MinkowskiModuleBase):
             )
             state = LIFFeedForwardState(v_tensor.F, i_tensor.F)
             input = self.union(input, zero_state_tensor)
-        else:
-            zero_input_tensor = ME.SparseTensor(
-                features=torch.zeros_like(input.F),
-                coordinates=input.coordinates,
-                coordinate_manager=input.coordinate_manager,
-            )
-            v_tensor, i_tensor = zero_input_tensor, zero_input_tensor
-        output, s_out = self.module(input.F, state)
-        return (
-            ME.SparseTensor(
-                features=output,
-                coordinates=v_tensor.C,
-                coordinate_manager=input.coordinate_manager,
-            ),
-            MinkowskiLIFState(
+            """
+            lifstate = LIFFeedForwardState(state.v.F, state.i.F)
+
+            output, s_out = self.module(input.F, lifstate)
+
+            new_state = MinkowskiLIFState(
                 v=ME.SparseTensor(
                     features=s_out.v,
-                    coordinates=v_tensor.C,
+                    coordinates=state.v.C,
                     coordinate_manager=input.coordinate_manager,
                 ),
                 i=ME.SparseTensor(
                     features=s_out.i,
-                    coordinates=i_tensor.C,
+                    coordinates=state.i.C,
                     coordinate_manager=input.coordinate_manager,
                 ),
                 z=output,
+            )
+
+        else:
+            zero_input_tensor = ME.SparseTensor(
+                features=torch.zeros_like(input.F),
+                coordinates=input.C,
+                coordinate_manager=input.coordinate_manager,
+            )
+            v_tensor, i_tensor = zero_input_tensor, zero_input_tensor
+            lifstate = LIFFeedForwardState(v_tensor.F, i_tensor.F)
+
+            output, s_out = self.module(input.F, lifstate)
+
+            new_state = MinkowskiLIFState(
+                v=ME.SparseTensor(
+                    features=s_out.v,
+                    coordinates=input.C,
+                    coordinate_manager=input.coordinate_manager,
+                ),
+                i=ME.SparseTensor(
+                    features=s_out.i,
+                    coordinates=input.C,
+                    coordinate_manager=input.coordinate_manager,
+                ),
+                z=output,
+            )
+
+        return (
+            ME.SparseTensor(
+                features=output,
+                coordinates=input.C,
+                coordinate_manager=input.coordinate_manager,
             ),
+            new_state
         )
